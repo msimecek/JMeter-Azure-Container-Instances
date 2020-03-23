2cd4614f3653afd3247a4579c4c74458d0b960e5
Martin Simecek <msimecek@users.noreply.github.com>
2020-03-23 20:12:57 +0100

    Full path

--- infra/agents/agent-start.sh
+++ infra/agents/agent-start.sh
@@ -2,4 +2,4 @@
 
 source /home/adminuser/.profile
 
-$JMETER_HOME/bin/jmeter-server -Dserver.rmi.localport=50000 -Dserver_port=1099 -Dserver.rmi.ssl.disable=true
+/jmeter/apache-jmeter-5.2.1/bin/jmeter-server -Dserver.rmi.localport=50000 -Dserver_port=1099 -Dserver.rmi.ssl.disable=true
